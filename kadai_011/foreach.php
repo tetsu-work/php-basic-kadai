67ffa8e25c1fe8665c653bbafee5ab47afa97b5c
tetsu-work <teppei1409@gmail.com>
2025-09-17 04:45:33 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_011</title>
+</head>
+<body>
+  <p>
+    <?php
+$vegetable = array(
+    '名前' => '玉ねぎ',
+    '値段' => 200,
+    '産地' => '北海道'
+);
+
+foreach ($vegetable as $key => $value) {
+    echo $key . "：" . $value . "<br>"; 
+}
+?>
+  </p>
+
+</body>
